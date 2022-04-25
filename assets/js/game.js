44b0c955ff9658f62270219ba6f5eadaf0910300
Cheddar Bay Biscuits <nowaczewskijack@gmail.com>
2022-04-25 11:13:03 -0400

    Added javascript to game.js for foundation of game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript os running!");
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
+
+// window prompt robots name
+var playerName = window.prompt("What is your robots name?");
+
+console.log(playerName);
+// do math and log 20
+console.log(10+10);
+console.log("Our Robot's name is " + playerName);
